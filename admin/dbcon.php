21c89a6426135cef41547b6621eca9328b6df952
Mo Alaklouk <61121384+MoAlaklouk@users.noreply.github.com>
2023-06-02 00:51:11 +0300

    edit

--- admin/dbcon.php
+++ admin/dbcon.php
@@ -1,3 +1,3 @@
 <?php
-$conn = mysqli_connect('localhost','root','','capstone') or die(mysqli_error());
+$conn = mysqli_connect('us-cdbr-east-06.cleardb.net','b3866a946a25e9','a99a071b ','heroku_8f15f863c738f6a') or die(mysqli_error());
 ?>
