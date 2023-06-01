a3533a0b36896a28fb31a3ab472da26eb343b04d
Mo Alaklouk <101597758+nadaOsama20@users.noreply.github.com>
2023-06-02 01:32:19 +0300

    s

--- admin/dbcon.php
+++ admin/dbcon.php
@@ -1,4 +1,8 @@
-<!-- <?php
-// $conn = mysqli_connect('us-cdbr-east-06.cleardb.net','b3866a946a25e9','a99a071b ','heroku_8f15f863c738f6a') or die(mysqli_error());
+<?php
+        $host = 'us-cdbr-east-06.cleardb.net';
+        $db = 'heroku_8f15f863c738f6a';
+        $user = 'b3866a946a25e9';
+        $pass = 'a99a071b';
+$conn = mysqli_connect($host, $user, $pass,$db);
 
-?> -->
+?>
